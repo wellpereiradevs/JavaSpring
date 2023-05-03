e17f018b675a04ffe2719f2e84841c92dfeaf7ab
wellpereiradevs <wellpereiradevs@gmail.com>
2023-05-03 10:14:24 -0300

    First Commit

---
+++ java/src/test/java/com/devsuperior/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
